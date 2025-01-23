80048ea2fa7de82ae723d2bb50bce78a6534335c
Damayanti-ok <damayantieka.yp@gmail.com>
2025-01-23 12:01:27 +0700

    Update Script Pemain

--- Assets/Scripts/Pemain.cs
+++ Assets/Scripts/Pemain.cs
@@ -9,8 +9,8 @@ public class Pemain : MonoBehaviour
     public List<TanganPemain> ListTanganPemain = new List<TanganPemain>();
     public int Skor;
     public List<TanganPemain> KartuPilihan = new List<TanganPemain>();
-    //public ManagerKartu ManagerKartu;
-    //public AlurGame AlurGame;
+    public ManagerKartu ManagerKartu;
+    public AlurGame AlurGame;
     public Button ButtonBuangKartu;
     public Button ButtonKombinasi;
     public Button ButtonTambahWaktu;
@@ -19,62 +19,142 @@ public class Pemain : MonoBehaviour
 
     private void OnEnable()
     {
-        
+        for (int index = 0; index < ListTanganPemain.Count; index++)
+        { 
+        ListTanganPemain[index].EventPilihKartu.AddListener(PilihKartu(TanganPemain kartu));
+        }
     }
 
     private void OnDisable()
     {
-
+        for (int index = 0; index < ListTanganPemain.Count; index++)
+        {
+            ListTanganPemain[index].EventPilihKartu.RemoveListener(PilihKartu(TanganPemain kartu));
+        }
     }
 
     public void AmbilKartuAwalan()
     {
-
+        Kartu kartu = ManagerKartu.AmbilKartu();
+        
+        for (int index = 0; index < ListTanganPemain.Count; index++)
+        {
+            GantiKartu(index, kartu);
+        }
+        
     }
 
     public void MulaiBermain()
     {
+       
+        AmbilKartuAwalan();
+        MulaiMemilihKartu();
 
     }
 
     public void MulaiMemilihKartu()
     {
-
+        for (int index = 0; index < ListTanganPemain.Count; index++)
+        {
+            ListTanganPemain[index].SimpanKartu();
+        }
     }
 
-    //public void GantiKartu(int indexKartu, Kartu kartuBaru)
-    //{ 
-
-    //}
+    public void GantiKartu(int indexKartu, Kartu kartuBaru)
+    {
+        ListTanganPemain[indexKartu].UpdateDataKartu(kartuBaru);
+    }
 
     public void PilihKartu(TanganPemain kartu)
     {
+        kartu.UbahKartuDipilih(!kartu.Kartudipilih);
 
+        if(kartu.kartuDipilih == true)
+        {
+            kartu.PilihKartu();
+            KartuPilihan.Add(kartu);
+        }
+        else
+        {
+            kartu.SimpanKartu();
+            KartuPilihan.Remove(kartu);
+        }
+
+        HitungSkorKartu();
     }
 
     public void HitungSkorKartu()
     {
+        Skor = 0;
+        if (KartuPilihan.Count > 0)
+        {
+            for (int index = 0; index < KartuPilihan.Count; index++)
+            {
+                Skor = Skor + KartuPilihan[index].DataKartu.Skor;
+            }
 
+            EvaluasiKartu();
+        }
+        else
+        {
+            EvaluasiKartu();
+        }
     }
 
     public void EvaluasiKartu()
     {
+        if (Skor == 10)
+        {
+            if(KartuPilihan.Count == 3)
+            {
+                ButtonTukarKartu.interactable = false;
+                ButtonKombinasi.interactable = true;
+                ButtonTambahWaktu.interactable = false;
+            }
+            else if(KartuPilihan.Count == 2)
+            {
+                ButtonTukarKartu.interactable = false;
+                ButtonKombinasi.interactable = false;
+                ButtonTambahWaktu.interactable = true;
+            }
+        }
 
+        else
+        {
+            if (KartuPilihan.Count == 1)
+            {
+                ButtonBuangKartu.interactable = true;
+                ButtonKombinasi.interactable = false;
+                ButtonTambahWaktu.interactable = false;
+            }
+
+            else
+            {
+                ButtonBuangKartu.interactable = false;
+                ButtonKombinasi.interactable = false;
+                ButtonTambahWaktu.interactable = false;
+            }
+        }
     }
 
     public void BuangKartu()
     {
-
+        int banyakKartu = KartuPilihan.Count;
+        AlurGame.KurangiWaktu();
+        TukarKartu();
     }
 
     public void KumpulkanKombinasi()
     {
-
+        ButtonKombinasi.interactable = false;
+        AlurGame.Menang();
     }
 
     public void TambahWaktu()
     {
-
+        int banyakKartu = KartuPilihan.Count;
+        AlurGame.TambahWaktu();
+        TukarKartu();
     }
 
     public void TukarKartu()
