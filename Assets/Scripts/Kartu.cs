ef0bf7a064382d76354855af353d6832c6a08e54
Okti <oktinindyati@gmail.com>
2025-01-23 11:31:31 +0700

    Tugas 1 - Membuat Struktur Data Kartu
    Tugas 1 - Membuat Struktur Data Kartu

---
+++ Assets/Scripts/Kartu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kartu : MonoBehaviour
+{
+    public string Name;
+    public TipeKartu Tipe;
+    public int Skor;
+    public MeshRenderer MeshKartu;
+    public Material MaterialKartu;
+}
