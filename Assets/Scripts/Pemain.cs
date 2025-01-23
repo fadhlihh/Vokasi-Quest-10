8bf150db067dac00d6aab2ac430f4a524daa8461
Damayanti-ok <damayantieka.yp@gmail.com>
2025-01-23 11:20:56 +0700

    Script Pemain dan TanganPemain

---
+++ Assets/Scripts/Pemain.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Pemain : MonoBehaviour
+{
+    public List<TanganPemain> ListTanganPemain = new List<TanganPemain>();
+    public int Skor;
+    public List<TanganPemain> KartuPilihan = new List<TanganPemain>();
+    //public ManagerKartu ManagerKartu;
+    //public AlurGame AlurGame;
+    public Button ButtonBuangKartu;
+    public Button ButtonKombinasi;
+    public Button ButtonTambahWaktu;
+    public GameObject PopUpInfo;
+    public TMP_Text TextPopUpInfo;
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+
+    }
+
+    public void AmbilKartuAwalan()
+    {
+
+    }
+
+    public void MulaiBermain()
+    {
+
+    }
+
+    public void MulaiMemilihKartu()
+    {
+
+    }
+
+    //public void GantiKartu(int indexKartu, Kartu kartuBaru)
+    //{ 
+
+    //}
+
+    public void PilihKartu(TanganPemain kartu)
+    {
+
+    }
+
+    public void HitungSkorKartu()
+    {
+
+    }
+
+    public void EvaluasiKartu()
+    {
+
+    }
+
+    public void BuangKartu()
+    {
+
+    }
+
+    public void KumpulkanKombinasi()
+    {
+
+    }
+
+    public void TambahWaktu()
+    {
+
+    }
+
+    public void TukarKartu()
+    {
+
+    }
+
+    public void SelesaiMemilihKartu()
+    {
+
+    }
+
+    public void MunculkanPopUpInfo(string pesan)
+    {
+
+    }
+}
---
+++ Assets/Scripts/TanganPemain.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TanganPemain : MonoBehaviour
+{
+    //public Kartu DataKartu;
+    public MeshRenderer MeshRendererKartu;
+    public MeshFilter MeshFilterKartu;
+    public Color WarnaNormal;
+    public Color WarnaHighlight;
+    public bool KartuDipilih;
+    public UnityEvent<TanganPemain> EventPilihKartu;
+
+
+    public void OnMouseDown()
+    {
+
+    }
+
+    //public void UpdateDataKartu(Kartu dataKartuBaru)
+    //{
+
+    //}
+
+    public void PilihKartu()
+    {
+
+    }
+
+    public void SimpanKartu()
+    {
+
+    }
+
+    public void UbahKartuDipilih(bool statusKartuDipilih)
+    {
+
+    }
+}
